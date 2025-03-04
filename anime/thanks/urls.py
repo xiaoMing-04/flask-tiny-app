1b7c0cd0a45c982526410566e967ce0f9c851eb3
Minh <minhnguyen47431@gmail.com>
2025-03-04 14:27:06 +0700

    secret feature

--- anime/thanks/urls.py
+++ anime/thanks/urls.py
@@ -3,7 +3,7 @@ from .views import thanks, serve_video, so_close_so_far, memorable
 
 urlpatterns = [
     # path('thanks/', thanks, name='thanks'),
-    path('video/', serve_video, name='serve_video'),
+    path('thanks/', thanks, name='thanks'),
     path('thanks/so_close_so_far/', so_close_so_far, name='so_close_so_far'),
     path('thanks/memorable/', memorable, name='memorable')
 ]
